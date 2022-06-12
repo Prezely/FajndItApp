57cd354543fe24ca8d672967df6319cb3286c482
Prezely <92024342+Prezely@users.noreply.github.com>
2022-06-12 19:57:06 +0200

    Brez Baze + Search (ne dela)

--- app/src/main/java/si/uni_lj/fe/tnuv/fajndit2/CustomAdapter.java
+++ app/src/main/java/si/uni_lj/fe/tnuv/fajndit2/CustomAdapter.java
@@ -4,31 +4,32 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.zip.Inflater;
 
-public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHolder> {
+public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHolder> implements Filterable {
 
     private Context context;
-    private ArrayList izdelek_id,  izdelek_ime, izdelek_kategorija, izdelek_cena, izdelek_slika;
+
+    private List<Izdelek> vsiIzdelki;
+
+    private List<Izdelek> izbraniIzdelki;
+
+
 
     CustomAdapter(Context context,
-                  ArrayList izdelek_id,
-                  ArrayList izdelek_ime,
-                  ArrayList izdelek_kategorija,
-                  ArrayList izdelek_cena,
-                  ArrayList izdelek_slika){
+                  List<Izdelek> vsiIzdelki){
         this.context = context;
-        this.izdelek_id = izdelek_id;
-        this.izdelek_ime = izdelek_ime;
-        this.izdelek_kategorija = izdelek_kategorija;
-        this.izdelek_cena = izdelek_cena;
-        this.izdelek_slika = izdelek_slika;
+        this.vsiIzdelki = vsiIzdelki;
+        izbraniIzdelki = new ArrayList<>(vsiIzdelki);
     }
 
     @NonNull
@@ -42,16 +43,16 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHold
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-        holder.izdelek_id_txt.setText(String.valueOf(izdelek_id.get(position)));
-        holder.izdelek_ime_txt.setText(String.valueOf(izdelek_ime.get(position)));
-        holder.izdelek_kategorija_txt.setText(String.valueOf(izdelek_kategorija.get(position)));
-        holder.izdelek_cena_txt.setText(String.valueOf(izdelek_cena.get(position)));
-        holder.izdelek_slika_txt.setText(String.valueOf(izdelek_slika.get(position)));
+        holder.izdelek_id_txt.setText(String.valueOf(vsiIzdelki.get(position).getId()));
+        holder.izdelek_ime_txt.setText(String.valueOf(vsiIzdelki.get(position).getIme()));
+        holder.izdelek_kategorija_txt.setText(String.valueOf(vsiIzdelki.get(position).getKategorija()));
+        holder.izdelek_cena_txt.setText(String.valueOf(vsiIzdelki.get(position).getCena()));
+        holder.izdelek_slika_txt.setText(String.valueOf(vsiIzdelki.get(position).getSlika()));
     }
 
     @Override
     public int getItemCount() {
-        return izdelek_id.size();
+        return vsiIzdelki.size();
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
@@ -67,4 +68,49 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHold
 
         }
     }
+
+    @Override
+    public Filter getFilter() {
+        return filterIzdelkov;
+    }
+
+    private Filter filterIzdelkov = new Filter() {
+        @Override
+        protected FilterResults performFiltering(CharSequence charSequence) {
+
+            List<Izdelek> filtriraniIzdelki = new ArrayList<>();
+
+            if (charSequence == null || charSequence.length() == 0) {
+                filtriraniIzdelki.addAll(izbraniIzdelki);
+            }
+            else {
+                String filterPattern = charSequence.toString().toLowerCase().trim();
+
+                for (Izdelek izdelek : izbraniIzdelki) {
+
+                    if (izdelek.getIme().toLowerCase().contains(filterPattern)) {
+                        filtriraniIzdelki.add(izdelek);
+                    }
+                }
+            }
+
+            FilterResults results = new FilterResults();
+            results.values = filtriraniIzdelki;
+            return results;
+        }
+
+        @Override
+        protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+            izbraniIzdelki.clear();
+
+            System.out.println("Filtriraj");
+
+
+            izbraniIzdelki.addAll((List) filterResults.values);
+
+            System.out.println(filterResults.values);
+
+            notifyDataSetChanged();
+        }
+    };
 }
--- app/src/main/java/si/uni_lj/fe/tnuv/fajndit2/MainActivity.java
+++ app/src/main/java/si/uni_lj/fe/tnuv/fajndit2/MainActivity.java
@@ -6,13 +6,19 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.SearchView;
 import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,7 +26,12 @@ public class MainActivity extends AppCompatActivity {
     FloatingActionButton add_button;
 
     MyDatabseHelper myDB;
-    ArrayList<String> izdelek_id, izdelek_ime, izdelek_kategorija, izdelek_cena, izdelek_slika;
+
+    ArrayList<String> izdelek_id;
+    ArrayList<String> izdelek_ime;
+    ArrayList<String> izdelek_kategorija;
+    ArrayList<String> izdelek_cena;
+    ArrayList<String> izdelek_slika;
 
     CustomAdapter customAdapter;
 
@@ -28,6 +39,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<Izdelek> vsiIzdelki = new ArrayList<Izdelek>();
+
+        vsiIzdelki.add(new Izdelek("1", "Sok", "Kategorija1", 1, "Slika1"));
+        vsiIzdelki.add(new Izdelek("2", "Mleko", "Kategorija2", 2, "Slika2"));
+        vsiIzdelki.add(new Izdelek("3", "Cokolada", "Kategorija3", 3, "Slika3"));
 
         recView = findViewById(R.id.recycleView);
         add_button = findViewById(R.id.add_button);
@@ -46,11 +63,52 @@ public class MainActivity extends AppCompatActivity {
         izdelek_cena = new ArrayList<>();
         izdelek_slika = new ArrayList<>();
 
-        storeDataInArrays();
+        //storeDataInArrays();
 
-        customAdapter = new CustomAdapter(MainActivity.this, izdelek_id, izdelek_ime, izdelek_kategorija, izdelek_cena, izdelek_slika);
+        izdelek_id.add("1");
+        izdelek_ime.add("Sok");
+        izdelek_kategorija.add("Pijača");
+        izdelek_cena.add("1.99");
+        izdelek_slika.add("Slika1");
+
+        izdelek_id.add("2");
+        izdelek_ime.add("Mleko");
+        izdelek_kategorija.add("Mlečni Izdelki");
+        izdelek_cena.add("2.99");
+        izdelek_slika.add("Slika2");
+
+        izdelek_id.add("3");
+        izdelek_ime.add("Čokolada");
+        izdelek_kategorija.add("Prigrizki");
+        izdelek_cena.add("3.99");
+        izdelek_slika.add("Slika3");
+
+        customAdapter = new CustomAdapter(MainActivity.this, vsiIzdelki/*izdelek_id, izdelek_ime, izdelek_kategorija, izdelek_cena, izdelek_slika*/);
         recView.setAdapter(customAdapter);
         recView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.meni, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                customAdapter.getFilter().filter(s);
+                return true;
+            }
+        });
+        return true;
     }
 
     void storeDataInArrays(){
@@ -65,7 +123,6 @@ public class MainActivity extends AppCompatActivity {
                 izdelek_kategorija.add(cursor.getString(2));
                 izdelek_cena.add(cursor.getString(3));
                 izdelek_slika.add(cursor.getString(4));
-
             }
         }
     }
